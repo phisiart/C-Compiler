ff3749d2efe9e77ec106937cee35b0a548b1491e
Johnson Tan <phisiart@gmail.com>
2015-08-23 17:57:55 +0100

    Update README

--- driver/Program.cs
+++ driver/Program.cs
@@ -16,39 +16,7 @@ namespace driver {
 
             if (args.Count() == 0) {
                 String src = @"
-int printf(char *, int);
-void *malloc(unsigned int nbytes);
-
-typedef struct Node {
-    int value;
-    struct Node *next;
-} Node;
-
-Node *cons(int value, Node *tl) {
-    Node *hd = malloc(sizeof(struct Node));
-    hd->value = value;
-    hd->next = tl;
-    return hd;
-}
-
-void print_list(Node *hd) {
-    Node *cur = hd;
-    for (; cur != (void *)0; cur = cur->next) {
-        printf(""%d\t"", cur->value);
-    }
-}
-
-void print_list_recursive(Node *hd) {
-    if (hd != (void *)0) {
-        printf(""%d\t"", hd->value);
-        print_list_recursive(hd->next);
-    }
-}
-
 int main() {
-    Node *hd = cons(2, cons(1, cons(0, (void *)0)));
-    print_list(hd);
-    print_list_recursive(hd);
     return 0;
 }
 ";
