7900e09ef5698cd1a5f9b34b16f065b31cbdc20e
Johnson Tan <phisiart@gmail.com>
2015-08-26 22:57:28 +0100

    clean_all

--- TestPrograms/clean_all.py
+++ TestPrograms/clean_all.py
@@ -2,7 +2,7 @@
 
 import os
 
-file_names = [file_name for file_name in os.listdir('.') if not file_name.endswith('.c')]
+file_names = [file_name for file_name in os.listdir('.') if not file_name.endswith('.c') and not file_name.endswith('.py')]
 
 for file_name in file_names:
     os.system('rm %s' % file_name)
